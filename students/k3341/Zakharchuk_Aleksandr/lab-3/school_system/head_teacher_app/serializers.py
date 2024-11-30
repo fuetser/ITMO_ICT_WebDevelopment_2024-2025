865fec431a4f323ef9c1996f5b2e53b224a1034d
Alexander Zakharchuk <mystang.pro@gmail.com>
2024-11-30 19:54:59 +0300

    Added nested serializers

--- students/k3341/Zakharchuk_Aleksandr/lab-3/school_system/head_teacher_app/serializers.py
+++ students/k3341/Zakharchuk_Aleksandr/lab-3/school_system/head_teacher_app/serializers.py
@@ -10,6 +10,9 @@ class RoomSerializer(serializers.ModelSerializer):
 
 
 class TeacherSerializer(serializers.ModelSerializer):
+    assigned_room_id = serializers.PrimaryKeyRelatedField(queryset=models.Room.objects.all(), source="assigned_room")
+    assigned_room = RoomSerializer(read_only=True)
+
     class Meta:
         model = models.Teacher
         fields = "__all__"
@@ -22,25 +25,44 @@ class SubjectSerializer(serializers.ModelSerializer):
 
 
 class ClassSerializer(serializers.ModelSerializer):
+    homeroom_teacher_id = serializers.PrimaryKeyRelatedField(queryset=models.Teacher.objects.all(), source="homeroom_teacher")
+    homeroom_teacher = TeacherSerializer(read_only=True)
+
     class Meta:
         model = models.Class
-        fields = ['class_id', 'class_name', 'homeroom_teacher_id']
         fields = "__all__"
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    student_class_id = serializers.PrimaryKeyRelatedField(queryset=models.Class.objects.all(), source="student_class")
+    student_class = ClassSerializer(read_only=True)
+
     class Meta:
         model = models.Student
         fields = "__all__"
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    student_class_id = serializers.PrimaryKeyRelatedField(queryset=models.Class.objects.all(), source="student_class")
+    student_class = ClassSerializer(read_only=True)
+    subject_id = serializers.PrimaryKeyRelatedField(queryset=models.Subject.objects.all(), source="subject")
+    subject = SubjectSerializer(read_only=True)
+    teacher_id = serializers.PrimaryKeyRelatedField(queryset=models.Teacher.objects.all(), source="teacher")
+    teacher = TeacherSerializer(read_only=True)
+    room_number_id = serializers.PrimaryKeyRelatedField(queryset=models.Room.objects.all(), source="room_number")
+    room_number = RoomSerializer(read_only=True)
+
     class Meta:
         model = models.Schedule
         fields = "__all__"
 
 
 class GradeSerializer(serializers.ModelSerializer):
+    student_id = serializers.PrimaryKeyRelatedField(queryset=models.Student.objects.all(), source="student")
+    student = StudentSerializer(read_only=True)
+    subject_id = serializers.PrimaryKeyRelatedField(queryset=models.Subject.objects.all(), source="subject")
+    subject = SubjectSerializer(read_only=True)
+
     class Meta:
         model = models.Grade
         fields = "__all__"
