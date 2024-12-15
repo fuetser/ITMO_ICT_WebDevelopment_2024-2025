074d98e2aaca91afab6e54d982f49a04706e2539
Alexander Zakharchuk <mystang.pro@gmail.com>
2024-12-15 20:56:44 +0300

    Set up cors

--- students/k3341/Zakharchuk_Aleksandr/lab-3/school_system/school_system/settings.py
+++ students/k3341/Zakharchuk_Aleksandr/lab-3/school_system/school_system/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "drf_spectacular",
+    "corsheaders",
     "head_teacher_app",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -137,3 +139,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
